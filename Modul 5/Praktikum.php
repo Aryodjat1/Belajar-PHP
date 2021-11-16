4f4fec8b1603fcbc6de2a1107a70b0f87ab18978
Aryodjat1 <aryodjati5@gmail.com>
2021-11-16 20:52:52 +0700

    Modul 5

---
+++ Modul 5/Praktikum.php
@@ -0,0 +1,235 @@
+<?php
+    function luasKubus(){
+        $luas = "sisi * 6";
+        echo "Rumus luas Lingkaran adalah = . $luas. <br>";
+    }
+
+    function volumeKubus(){
+        $volume = "Sisi * Sisi * Sisi";
+        echo "Rumus volume kubus adalah = . $volume"; 
+    }
+
+    
+    luasKubus();
+    volumeKubus();
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<body>
+    <form action="" method="post">
+        <option value="1" name="luaskubus">Luas Kubus</option>
+    </form>
+        
+    <?php
+
+function LuasSegitiga()
+{
+    # code...
+}
+
+if ($_POST) {
+    $segitiga = $_POST['luaskubus'];
+    if (isset($segitiga)) {
+        
+    }
+}
+?>
+</body> -->
